2bc7a1a31a36fed8e8a3ad3047789f67b60775b7
adralak <yan.garito@ns-rennes.fr>
2020-01-23 09:33:08 +0100

    Done with minor functions; missing main one

--- 1/src/TP1RtlFlowGraph.java
+++ 1/src/TP1RtlFlowGraph.java
@@ -6,62 +6,94 @@ import java.util.HashMap;
 
 public class TP1RtlFlowGraph extends FlowGraph {
 
-	public HashMap<Node,Data> node_data;
-	public HashMap<Instr,Node> instr_node;
+     private HashMap<Node,Data> node_data;
+     private HashMap<Instr,Node> instr_node;
+     private HashMap<EndInstr,Node> end_instr_node;
+     private Node first;
+     private Block e;
+
+     public Node entry() {
+	  return first;
+     }
+
+     public Object instr(Node n) {
+	  Data d = node_data[n];
+
+	  if(d.end)
+	       return d.e;
+	  else if(n == first)
+	       return e;
+	  else 
+	       return d.i;
+     }
+
+     public TP1RtlFlowGraph(Function f) {
+	  node_data = new HashMap<Node,Data>();
+	  instr_node = new HashMap<Instr,Node>();
+	  end_instr_node = new HashMap<EndInstr,Node>();
+
+	  e = f.getEntry();
+	  first = new Node();
+	  Data d = new Data(f.params);
+	  node_data.put(first, d);
+     }
+
+     public List<Ident> def(Node node) {
+	  return node_data[node].def;
+     }
+
+     public List<Ident> use(Node node) {
+	  return node_data[node].use;
+     }
+
+     public Node node(Instr i) {
+	  return instr_node[i];
+     }
+
+     public Node node(EndInstr i) {
+	  return end_instr_node[i];
+     }
+
+     public class Data {
+	  public List<Ident> use;
+	  public List<Ident> def;
+	  public boolean end;
+	  public Instr i;
+	  public EndInstr e;
+
+	  public Data(Instr _i) {
+	       use = new List<Ident>();
+	       def = new List<Ident>();
+	       i = _i;
+	       end = false;
+	  }
+
+	  public Data(EndInstr _e)
+	       {
+		    use = new List<Ident>();
+		    def = new List<Ident>();
+		    e = _e;
+		    end = true;
+	       }
+
+	  Public Data(List<Ident> def)
+	       {
+		    use = new List<Ident>();
+		    this.def = def;
+	       }
+
+     }
+
+     public class TP1Visitor implements InstrVisitor<Data> {
+
+	  public Data visit(Assign a) {
+	       Ident _ident = a.ident;
+	       Operand _operand = a.operand;
+			
+	  }
 
 
-	public Node entry() {
-		return null; //TODO
-	}
-
-	public Object instr(Node n) {
-		return null; //TODO
-	}
-
-	public TP1RtlFlowGraph(Function f) {
-		 node_data = new HashMap<Node,Data>();
-		 instr_node = new HashMap<Instr,Node>();
-	}
-
-	public Set<Ident> def(Node node) {
-		return null; //TODO
-	}
-
-	public Set<Ident> use(Node node) {
-		return null; //TODO
-	}
-
-	public Node node(Instr i) {
-		return null; //TODO
-	}
-
-	public Node node(EndInstr i) {
-		return null; //TODO
-	}
-
-	public class Data {
-		public Set<Ident> use;
-		public Set<Ident> def;
-		public Instr i;
-
-		public Data(Instr _i) {
-			use = new Set<Ident>();
-			def = new Set<Ident>();
-			Instr = _i; 
-		}
-
-	}
-
-	public class TP1Visitor implements InstrVisitor<Data> {
-
-		public Data visit(Assign a) {
-			Ident _ident = a.ident;
-			Operand _operand = a.operand;
-
-		}
-
-
-	}
+     }
 
 
 }
