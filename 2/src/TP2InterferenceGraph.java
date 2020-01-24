33871ce0e5a3832bcc85e63ef324fd909e3f56de
adralak <yan.garito@ns-rennes.fr>
2020-01-24 12:08:38 +0100

    Naive version of interference graph done

--- 2/src/TP2InterferenceGraph.java
+++ 2/src/TP2InterferenceGraph.java
@@ -12,7 +12,8 @@ import rtl.graph.Graph.Node;
  */
 public class TP2InterferenceGraph extends AbstractInterferenceGraph {
 
-	//TODO
+     	private Map<Ident,Node> id_to_node = new Hashtable<Ident,Node>();
+     	private Map<Node,Ident> node_to_id = new Hashtable<Node,Ident>();
 
 	/**
 	 * Construit un graphe d'interference à partir d'un {@link rtl.graph.FlowGraph}
@@ -21,23 +22,59 @@ public class TP2InterferenceGraph extends AbstractInterferenceGraph {
 	 * @param live Information du temps de vie des variables du programme.
 	 */
 	public TP2InterferenceGraph(FlowGraph g, AbstractLiveness live) {
-		//TODO
+	     Node x, y;
+	     
+	     for(Node n : g.nodes())
+	     {
+		  for(Ident id : g.use(n))
+		  {
+		       x = get_node(id);
+
+		       for(Ident other_id : live.liveOut(n))
+		       {
+			    if(!id.equals(other_id))
+			    {
+				 y = get_node(other_id);
+
+				 if(!x.adj(y))
+				 {
+				      addEdge(x, y);
+				      addEdge(y, x);
+				 }
+			    }
+		       }
+		  }
+	     }
 	}
 
+     private Node get_node(Ident id)
+	  {
+	       if(id_to_node.containesKey(id))
+		    return id_to_node.get(id);
+	       else
+	       {
+		    Node n = new Node();
+		    id_to_node.put(id, n);
+		    node_to_id.put(n, id);
+
+		    return n;
+	       }
+	  }
+     
 	/**
 	 * @param n Un noeud du graphe d'interference.
 	 * @return L'identifiant de la variable associée à ce noeud.
 	 */
 	public Ident ident(Node n) { 
-		return null; //TODO
+	     return node_to_id.get(n);
 	}
 	
 	/**
 	 * @param n Un identifiant de variable.
 	 * @return Le noeud associé à cette variable.
 	 */
-	public Node node(Ident id) { 
-		return null; //TODO
+	public Node node(Ident id) {
+	     return id_to_node.get(id);
 	}
 		
 }
