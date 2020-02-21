79966a8e771f9814d9762cd296d2c03e63278d4c
PaulBastide <paul.bastide@ens-rennes.fr>
2020-02-21 11:18:33 +0100

    onepass

--- 4/src/TP4AvailableExpressions.java
+++ 4/src/TP4AvailableExpressions.java
@@ -11,22 +11,22 @@ import rtl.graph.FlowGraph;
 
 /**
  * Analyse des expressions disponibles.
- * 
+ *
  * Associe à chaque noeud l'ensemble des expression disponibles.
  */
 public class TP4AvailableExpressions {
 
 	/**
 	 * Expression.
-	 * 
+	 *
 	 * On utilise une interface particulière pour représenter une expression
 	 * utilisée dans une instruction {@link rtl.Instr}.
-	 * 
+	 *
 	 * Cette interface est implémentée par les deux types d'expressions considérées
 	 * dans ce TP :
 	 * 	- Les expression arithmetiques et logiques ({@link rtl.BuiltIn} sauf {@code Alloc} et {@code Print}).
 	 *  - Les lectures en mémoire.
-	 *  
+	 *
 	 * Dans les deux cas, on ne s'intéresse qu'à la partie droite de l'affectation : l'expression.
 	 */
 	private static interface Expr {
@@ -43,13 +43,13 @@ public class TP4AvailableExpressions {
 		/**
 		 * Opérateur arithmetique ou logique.
 		 */
-		final String operator; 
-		
+		final String operator;
+
 		/**
 		 * Arguments de l'expression.
 		 */
 		final List<Operand> args;
-		
+
 		/**
 		 * Construction d'une expression.
 		 * @param operator L'opérateur arithmetique ou logique.
@@ -59,7 +59,7 @@ public class TP4AvailableExpressions {
 			this.operator = operator;
 			this.args = args;
 		}
-		
+
 		/**
 		 * Comparaison *logique* de deux expressions.
 		 */
@@ -68,21 +68,21 @@ public class TP4AvailableExpressions {
 			BuiltInExpr e = (BuiltInExpr) o;
 			return operator.equals(e.operator) && args.equals(e.args);
 		}
-		
+
 		@Override public int hashCode() {
 			return (operator.hashCode() + 31 * args.hashCode());
 		}
-		
-		@Override public String toString() {	
+
+		@Override public String toString() {
 			String s = args.toString();
-			return operator+"("+s.substring(1, s.length()-1)+")";	
-		}	
-		
+			return operator+"("+s.substring(1, s.length()-1)+")";
+		}
+
 		public boolean containsIdent(Ident id) {
 			return args.contains(id);
 		}
-	}	
-	
+	}
+
 	/**
 	 * Lecture en mémoire.
 	 */
@@ -90,7 +90,7 @@ public class TP4AvailableExpressions {
 		//Il vous faudra déterminer les champs d'instance
 		//Il vous faudra determiner les méthodes qui nécessitent une redéfinition
 		//TODO
-		
+
 		public boolean containsIdent(Ident id) {
 			return false; //TODO
 		}
@@ -100,22 +100,28 @@ public class TP4AvailableExpressions {
 	 * L'ensemble des expression disponibles en entrée de chaque noeud.
 	 */
 	private Map<Node,Set<Expr>> aeIn = new Hashtable<>();
-	
+
+	private Map<Node,Set<Expr>> oaeIn = new Hastable<>();
+
 	/**
 	 * L'ensemble des expression disponibles en sortie de chaque noeud.
 	 */
 	private Map<Node,Set<Expr>> aeOut = new Hashtable<>();
-	
+
+	private Map<Node,Set<Expr>> oaeOut = new Hashtable<>();
+
+
+
 	/**
 	 * Permet d'afficher les itérations de l'algorithme de calcul de point fixe.
 	 */
 	private final DataFlowDebug debug;
-	
+
 	/**
 	 * La fonction analysée.
 	 */
 	private Function f;
-	
+
 	/**
 	 * Le graphe de flot de controle de la fonction analysée.
 	 */
@@ -136,28 +142,90 @@ public class TP4AvailableExpressions {
 		Set<Expr> init = new HashSet<>();//TODO
 		//Attention cette initialisation est plus complexe que
 		//dans les TP précédents
-		
-		for (Node n : cfg.nodes()) { 
+		// On ajoute tout les expressions calculées dans le programme
+		for (Node n : cfg.nodes()) {
+			Instr instr = cfg.instr(n);
+			if (Instr != null) {
+				TP4InstrVisitor iv = new TP4InstrVisitor();
+				Expr tp_expr = instr.accept(iv);
+				if (tp_expr != null) {
+					init.add(tp_expr);
+				}
+			}
+		}
+
+		for (Node n : cfg.nodes()) {
 			aeIn.put(n, init);
-			aeOut.put(n, init);			
-		}	
+			aeOut.put(n, init);
+			oaeIn.put(n, init);
+			oaeOut.put(n, init);
+		}
+
 		onePass();
 		while (!isFixedPoint()) {
-			onePass();		
+			onePass();
 		}
 	}
 
 	private void onePass() {
 		for (Node n : cfg.nodes()) {
-			//TODO
-			//Il vous faudra relire et compléter (pour gen et kill) le slide 33 du CM4
-			//en vous aidant de l'exemple présenté slide 32
-		}		
+			oaeIn.put(n, aeIn.get(n));
+			oaeOut.put(n, aeOut.get(n));
+
+			Set<Expr> tempOut = new HashSet(oaeIn.get(n));
+			for (Ident id : cfg.def(n)) {
+				for (Expr e : oaeIn.get(n)) {
+					if (e.containsIdent(id)) {
+						tempOut.remove(e);
+					}
+				}
+			}
+
+			Instr instr = cfg.instr(n);
+			TP4InstrVisitor iv = new TP4InstrVisitor();
+			Expr tp_expr = instr.accept(iv);
+			if (tp_expr != null) {
+				tempOut.add(tp_expr);
+			}
+
+			Set<Expr> tempIn;
+			for (Node n_n : n.succ()) {
+				tempIn.addAll(oaeOut.get(n));
+			}
+
+			aeIn.put(n, tempIn);
+			aeOut.put(n, tempOut);
+		}
+
+
 		if (debug!=null) debug.recordCurrentMaps(aeIn, aeOut);
 	}
 
 	private boolean isFixedPoint() {
 		return true; //TODO
-	}	
-	
+	}
+
+	private	class TP4InstrVisitor implements InstrVisitor<Expr> {
+		public Instr visit(Assign a) {
+			return null;
+		}
+
+		public Instr visit(BuiltIn bi) {
+			return BuiltInExpr(bi.operator,bi.args);
+		}
+
+		public Instr visit(Call c) {
+			return null;
+		}
+
+		public Instr visit(MemRead mr) {
+			return null;
+		}
+
+		public Instr visit(MemWrite mw) {
+			return null;
+		}
+	}
+
+
 }
